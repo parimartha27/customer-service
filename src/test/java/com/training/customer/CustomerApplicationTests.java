e904c678242a7b1493cdf528bb1a219ac69a64e2
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2024-09-08 12:51:04 +0700

    fix: project name

--- src/test/java/com/training/customer/CustomerApplicationTests.java
+++ src/test/java/com/training/customer/CustomerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.training.product;
+package com.training.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductApplicationTests {
+class CustomerApplicationTests {
 
 	@Test
 	void contextLoads() {
